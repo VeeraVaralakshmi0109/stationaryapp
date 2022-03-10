83e17fb6f85c59a6feca37cfa46f0c26dafec4bd
VeeraVaralakshmi0109 <sallavaralakshmi.99@gmail.com>
2022-03-10 16:07:35 +0530

    added stationary files

---
+++ src/main/java/com/stationaryapp/StationaryBasic.java
@@ -0,0 +1,10 @@
+package com.stationaryapp;
+
+public class StationaryBasic {
+
+	public void stationaryBrand(String name) {
+		// String Name=name;
+		System.out.println(name);
+	}
+
+}
---
+++ src/test/java/com/stationaryapp/test/StationaryTest.java
@@ -0,0 +1,15 @@
+package com.stationaryapp.test;
+
+import com.stationaryapp.StationaryBasic;
+
+public class StationaryTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StationaryBasic brand = new StationaryBasic();
+		brand.stationaryBrand("abc");
+		brand.stationaryBrand("vara");
+
+	}
+
+}
